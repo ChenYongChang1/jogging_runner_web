b0f9436f79946b52d563ca9ea1049a8e14cb4831
陈永昌 <chenyongchang@bwcj.com>
2025-02-23 17:55:56 +0800

    feat: 新增各个页面路由

---
+++ src/pages/[lang]/post/[id].html.vue
@@ -0,0 +1,10 @@
+
+<template><div>nuxt</div></template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+div {
+  color: red;
+}
+</style>
---
+++ src/pages/[lang]/search.vue
@@ -0,0 +1,10 @@
+
+<template><div>search</div></template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+div {
+  color: red;
+}
+</style>
