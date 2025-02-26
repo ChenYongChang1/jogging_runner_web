b765d66c079d867596453619659a85b314bd01da
陈永昌 <chenyongchang@bwcj.com>
2025-02-26 22:37:48 +0800

    fix: 搜索页移动端

--- src/components/pages/search/ArticleSearchContent.vue
+++ src/components/pages/search/ArticleSearchContent.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="tw-w-[330px]">
+  <div class="ss:tw-w-full xl:tw-w-[330px]">
     <div
-      class="search-title tw-text-[36px] tw-font-[500] tw-text-basecolor tw-leading-[50px] tw-mb-[28px]"
+      class="search-title dd-title-text tw-font-[500] tw-text-basecolor tw-leading-[50px] xl:tw-mb-[28px] ss:tw-mb-[1.125rem]"
     >
       搜索
     </div>
-    <div class="tw-h-[64px] tw-w-full tw-mb-[68px]">
+    <div class="tw-h-[64px] tw-w-full xl:tw-mb-[68px] ss:tw-mb-[2.75rem]">
       <el-input class="tw-h-[68px] tw-w-full"></el-input>
     </div>
     <div
-      class="tw-text-[36px] tw-font-[500] tw-text-basecolor tw-leading-[50px] tw-mb-[28px]"
+      class="dd-title-text tw-font-[500] tw-text-basecolor tw-leading-[50px] tw-mb-[28px]"
     >
       最新文章
     </div>
@@ -22,24 +22,24 @@
     </div>
   </div>
   <div
-    class="tw-text-[36px] tw-font-[500] tw-text-basecolor tw-leading-[50px] tw-mb-[28px]"
+    class="dd-title-text tw-font-[500] tw-text-basecolor tw-leading-[50px] tw-mb-[28px]"
   >
     文章分类
   </div>
   <div class="article-list">
     <div class="article-rows tw-mb-[26px]">
       <div class="article-title tw-text-[24px] tw-font-[600] tw-leading-[38px] tw-mb-[12px] tw-text-basecolor">入门技巧</div>
-      <div class="article-type tw-pl-[18px] tw-text-[20px] tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
-      <div class="article-type tw-pl-[18px] tw-text-[20px] tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
-      <div class="article-type tw-pl-[18px] tw-text-[20px] tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
-      <div class="article-type tw-pl-[18px] tw-text-[20px] tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
+      <div class="article-type tw-pl-[18px] dd-fs-20 tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
+      <div class="article-type tw-pl-[18px] dd-fs-20 tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
+      <div class="article-type tw-pl-[18px] dd-fs-20 tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
+      <div class="article-type tw-pl-[18px] dd-fs-20 tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
     </div>
     <div class="article-rows tw-mb-[26px]">
       <div class="article-title tw-text-[24px] tw-font-[600] tw-leading-[38px] tw-mb-[12px] tw-text-basecolor">入门技巧</div>
-      <div class="article-type tw-pl-[18px] tw-text-[20px] tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
-      <div class="article-type tw-pl-[18px] tw-text-[20px] tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
-      <div class="article-type tw-pl-[18px] tw-text-[20px] tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
-      <div class="article-type tw-pl-[18px] tw-text-[20px] tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
+      <div class="article-type tw-pl-[18px] dd-fs-20 tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
+      <div class="article-type tw-pl-[18px] dd-fs-20 tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
+      <div class="article-type tw-pl-[18px] dd-fs-20 tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
+      <div class="article-type tw-pl-[18px] dd-fs-20 tw-font-[500] tw-text-text666 tw-mb-[12px]">基础知识</div>
     </div>
   </div>
 </template>
--- src/pages/[lang]/search.vue
+++ src/pages/[lang]/search.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="dd-container">
-    <div class="search-box tw-flex tw-pt-[56px]">
-      <div
-        class="search-content tw-w-[65.4%] tw-border-r-[1px] tw-border-linecolor tw-border-solid tw-pr-[30px]"
-      >
+    <div class="search-box lg:tw-flex xl:tw-pt-[56px] lg:tw-pt-[44px] ss:tw-pt-[32px]">
+      <div class="search-content">
         <bw-article-card class="card-row-item"></bw-article-card>
         <bw-article-card class="card-row-item"></bw-article-card>
       </div>
-      <div class="tw-w-[33.9%] tw-pl-[52px]">
+      <div class="lg:tw-w-[33.9%] xl:tw-pl-[52px] lg:tw-pl-[30px]">
         <article-search-content></article-search-content>
       </div>
     </div>
@@ -15,10 +13,14 @@
 </template>
 
 <script lang="ts" setup>
-import ArticleSearchContent from '~/components/pages/search/ArticleSearchContent.vue';
+import ArticleSearchContent from "~/components/pages/search/ArticleSearchContent.vue";
 </script>
 
 <style lang="scss" scoped>
+.search-content {
+  @apply lg:tw-w-[65.4%] lg:tw-border-r-[1px] lg:tw-border-linecolor lg:tw-border-solid lg:tw-pr-[30px];
+  @apply ss:tw-mb-[2.75rem];
+}
 .card-row-item + .card-row-item {
   margin-top: 44px;
 }
